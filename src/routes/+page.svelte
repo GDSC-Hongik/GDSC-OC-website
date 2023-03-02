2b89b1fe8c808373d84ce3c008b6457d1955df44
developomp <developomp@gmail.com>
2023-03-02 13:56:42 +0900

    refactor: code separation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,23 @@
 <script lang="ts">
-	import { dev } from "$app/environment"
-	import firebaseConfig from "$lib/firebaseConfig.json"
+	import { authStore, login, logout } from "../stores/authStore"
 	import { toast } from "@zerodevx/svelte-toast"
-	import { type FirebaseApp, initializeApp } from "firebase/app"
-	import {
-		type Auth,
-		GithubAuthProvider,
-		browserSessionPersistence,
-		connectAuthEmulator,
-		getAuth,
-		setPersistence,
-		signInWithRedirect,
-	} from "firebase/auth"
 	import { Button, P } from "flowbite-svelte"
-	import { onMount } from "svelte"
-
-	enum AuthState {
-		uninitialized,
-		signedIn,
-		notSignedIn,
-	}
-
-	let app: FirebaseApp
-	let auth: Auth
-	let authState: AuthState = AuthState.uninitialized
-
-	async function login() {
-		const provider = new GithubAuthProvider()
-		// https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps#available-scopes
-		provider.addScope("user:email")
-
-		await signInWithRedirect(auth, provider)
-	}
 
 	/**
 	 * AuthState가 signedIn일 시에만 작동
 	 */
 	async function copyUIDToClipboard() {
-		await navigator.clipboard.writeText(auth.currentUser?.uid || "")
-		toast.push("복사됨")
+		if (!$authStore.currentUser)
+			return toast.push("사용자 정보를 불러올 수 없습니다.", {
+				theme: {
+					"--toastBackground": "darkred",
+					"--toastBarBackground": "tomato",
+				},
+			})
+
+		await navigator.clipboard.writeText($authStore.currentUser.uid)
+		toast.push("코드가 복사되었습니다!")
 	}
-
-	onMount(async () => {
-		app = initializeApp(firebaseConfig)
-		auth = getAuth(app)
-		auth.onAuthStateChanged(() => {
-			authState = auth.currentUser ? AuthState.signedIn : AuthState.notSignedIn
-		})
-
-		if (dev) connectAuthEmulator(auth, "http://localhost:9099")
-
-		await setPersistence(auth, browserSessionPersistence)
-	})
 </script>
 
 <svelte:head>
@@ -59,16 +25,16 @@
 </svelte:head>
 
 <div class="m-5 flex flex-col gap-3">
-	{#if authState == AuthState.uninitialized}
-		<Button on:click={login}>GitHub 계정으로 등록</Button>
-	{:else if authState == AuthState.notSignedIn}
-		<Button on:click={login}>GitHub 계정으로 등록</Button>
-	{:else if authState == AuthState.signedIn}
+	{#if $authStore.isLoading}
+		<P>로딩중</P>
+	{:else if $authStore.currentUser}
 		<P size="2xl">
 			가입 완료. 디스코드 서버에서 "/등록" 명령어를 이용해 본인 인증 코드를
 			등록하세요.
 		</P>
 		<Button on:click={copyUIDToClipboard}>코드 복사</Button>
-		<Button on:click={login}>다른 GitHub 계정으로 가입</Button>
+		<Button on:click={logout}>로그아웃</Button>
+	{:else}
+		<Button on:click={login}>GitHub 계정으로 등록</Button>
 	{/if}
 </div>
---
+++ src/stores/authStore.ts
@@ -0,0 +1,36 @@
+import { auth } from "../lib/firebase/firebase.client"
+import {
+	GithubAuthProvider,
+	type User,
+	createUserWithEmailAndPassword,
+	signInWithRedirect,
+	signOut,
+} from "firebase/auth"
+import { type Writable, writable } from "svelte/store"
+
+export interface AuthStore {
+	isLoading: boolean
+	currentUser: User | null
+}
+
+export const authStore: Writable<AuthStore> = writable({
+	isLoading: true,
+	currentUser: null,
+})
+
+const provider = new GithubAuthProvider()
+
+export async function login() {
+	// https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps#available-scopes
+	provider.addScope("user:email")
+
+	await signInWithRedirect(auth, provider)
+}
+
+export async function signup(email: string, password: string) {
+	await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export async function logout() {
+	await signOut(auth)
+}
